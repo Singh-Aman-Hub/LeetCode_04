7b2c73404338d4c64716f340f1fa735628a5b9de
Aman Singh <amansingh102006@gmail.com>
2026-02-24 12:27:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (57.04%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
@@ -1,41 +1,18 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public static int ans =0;
-    public void calc(TreeNode root, String curr) {
-        if(root.left ==null && root.right==null){
-            String copy= curr;
-            curr+= String.valueOf(root.val);
-            // System.out.println(curr);
-            int val= Integer.parseInt(curr,2);
-            ans+= val;
-            curr= copy;
-            return;
-        }
-        curr+= String.valueOf(root.val);
-        
-        if(root.left!=null)calc(root.left,curr);
-        
-        if(root.right!= null )calc(root.right,curr);
-    }
-    
-    public int sumRootToLeaf(TreeNode root) {
-        ans=0;
-        calc(root,"");
-        return ans;
 
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode root, int curr) {
+        if (root == null) return 0;
+
+        curr = (curr << 1) | root.val;
+
+        if (root.left == null && root.right == null) {
+            return curr;
+        }
+
+        return dfs(root.left, curr) + dfs(root.right, curr);
     }
 }
