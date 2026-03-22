de6de932225e6d8f9d05558d3e5ad76779a63d92
Aman Singh <amansingh102006@gmail.com>
2026-03-22 21:58:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (54.23%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,40 +1,33 @@
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
-        int n= target.length;
-        boolean first= true;
-        boolean second = true;
-        boolean third= true;
-        boolean fourth= true;
+        if(compare(mat,target))return true;
+        int count=3;
+        while(count>0){
+            mat=rotate(mat);
+            if(compare(mat, target))return true;
+            count--;
+        }
+        return false;
+    }
+    private boolean compare(int[][] mat,int[][] target){
+        int n=mat.length;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if (mat[i][j]== target[j][n-i-1]  ){
-                    
-                }else if(first){
-                    first= !first;
+                if(mat[i][j]!=target[i][j]){
+                    return false;
                 }
-
-                if(mat[i][j]== target[n-i-1][n-j-1]){
-
-                }else if(second){
-                    // System.out.println(mat[i][j]+" "+target[n-i-1][n-j-1]);
-                    second= !second;
-                }
-
-                if(mat[i][j]== target[i][j]){
-
-                }else if(fourth){
-                    // System.out.println(mat[i][j]+" "+target[n-i-1][n-j-1]);
-                    fourth= !fourth;
-                }
-                if(mat[i][j]== target[n-j-1][i]){
-
-                }else if(third){
-                    // System.out.println(mat[i][j]+" "+target[n-i-1][n-j-1]);
-                    third= !third;
-                }
-                
             }
         }
-        return first|| second|| third || fourth;
+        return true;
+    }
+    private int[][]rotate(int[][]mat){
+        int n=mat.length;
+        int[][]res=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                res[n-1-j][i]=mat[i][j];
+            }
+        }
+        return res;
     }
 }
