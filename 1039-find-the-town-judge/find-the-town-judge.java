bddf3ef09a67c969e64a1d780b52233311dad2b3
Aman Singh <amansingh102006@gmail.com>
2026-04-17 01:14:16 +0530

    Time: 3 ms (73.13%) | Memory: 53.8 MB (23.63%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        if(n==1)return n;
+        int [] count= new int[n+1];
+        for(int [] vertex: trust){
+            count[vertex[0]]--;
+            count[vertex[1]]++;
+        }
+        for(int i=0;i<n+1;i++){
+            if(count[i]==n-1)return i;
+        }
+        return -1;
+
+    }
+}
